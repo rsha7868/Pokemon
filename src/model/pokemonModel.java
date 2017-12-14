72dd2fbcb5abba41f11daf9825f40782855b5a71
Sharp <rsha7868@740s-pl-127.local>
2017-12-14 13:29:15 -0700

    SAvving my pokemons

--- src/model/pokemonModel.java
+++ src/model/pokemonModel.java
@@ -1,6 +1,42 @@
 package model;
 
-public class pokemonModel
+public abstract class pokemonModel
 {
-
+	private int healthPoints;
+	private int attackPoints;
+	private double enhancementModifier;
+	private int number;
+	private String name;
+	private boolean canEvolve;
+	
+	public pokemonModel (int number, String name)
+	{
+		this.name = name;
+		this.number = number;
+	}
+	
+	public final String[] getPokemonTypes()
+	{
+		CLass<?> [] types = getClass().getInterfaces();
+		String [] pokeTypes = new String[types.length];
+		
+		for(int index = 0; index < types.length; index++)
+		{
+			String currentInterface = types[index].getCanonicalName();
+			currentInterface = currentInterface.replaceAll(this.getClass().getPackage().getName() + ".", "");
+			pokeTypes[index] = currentInterface;
+			}
+	}
+	public String toString()
+	{
+		String description = "Hi, I am a" + ", and my HP is " + healthPoints;
+		
+		return description;
+	}
+	public String getPokemonInformation()
+	{
+		String pokemonInfo = "This pokemon is of type: " + this.getClass().getSimpleName();
+		
+		return pokemonInfo;
+	}
 }
