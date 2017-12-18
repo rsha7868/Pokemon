0b58ee1b79a2ddd48a1f96617f5f72bab6aac320
Sharp <rsha7868@740s-pl-127.local>
2017-12-18 12:15:58 -0700

    Errors upon errors... Making more creatures.

---
+++ src/model/Stemicon.java
@@ -0,0 +1,25 @@
+package model;
+
+public class Stemicon extends pokemonModel implements Dragon, Wind
+{
+	public Stemicon(int number,String name)
+	{
+		super(number, name);
+	}
+	public String moveFlyable()
+	{
+		return "Hey I'm hovering.";
+	}
+	public String moveSpit()
+	{
+		return "Take my liquid boi.";
+	}
+	public String moveStomp()
+	{
+		return "You stuck under there?";
+	}
+	public String moveTornado()
+	{
+		
+	}
+}
--- src/model/errorBugger.java
+++ src/model/errorBugger.java
@@ -1,21 +1,9 @@
 package model;
 
-public class errorBugger extends pokemonModel implements Wind
+public class errorBugger extends Stemicon
 {
 	public errorBugger(int number, String name)
 	{
 		super(number, name);
-	}
-	public String moveTornado()
-	{
-		return "To the Skies.";
-	}
-	public String moveSteam()
-	{
-		return "Time to release Steam.";
-	}
-	public String moveDisapear()
-	{
-		return "You Can't See Me!!!";
 	}
 }
