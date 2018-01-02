fad9c1d5a3c50103b7c816901cf603d308e93089
Sharp <rsha7868@740s-pl-127.local>
2018-01-02 13:53:34 -0700

    Save.

--- src/view/PokemonPanel.java
+++ src/view/PokemonPanel.java
@@ -19,6 +19,8 @@ import controller.PokemonController;
 
 public class PokemonPanel extends JPanel
 {
+	public PokemonPanel() {
+	}
 	private PokemonController appController;
 	private SpringLayout appLayout;
 	
@@ -30,7 +32,7 @@ public class PokemonPanel extends JPanel
 	private JLabel modifierLabel;
 	private JLabel iconLabel;
 	
-	private JCheckBox evolveableBox;
+	private JCheckBox evolvableBox;
 	private JTextField nameField;
 	private JTextField numberField;
 	private JTextField attackField;
@@ -48,13 +50,46 @@ public class PokemonPanel extends JPanel
 	private JPanel secondType;
 	private JPanel thirdType;
 	private JPanel fourthType;
-}
-private void updatePokedexInfo(int index)
-{
-	nameField.setText(appController.getPokedex()get(index).getName());
+
+	private void updatePokedexInfo(int index)
+	{
+	nameField.setText(appController.getPokedex().get(index).getName());
 	evolvableBox.setSelected(appController.getPokedex().get(index).isCanEvolve());
-	numberField.setText(appController.getPokedex().get(index).getNumber( + "");
-	attackField.setText(appController.getPokedex().get(index).getAttackPoints( + "");
-	healthField.setText(appController.getPokedex().get(index).getHealthPoints( + "");
-	modifierField.setText(appController.getPokedex().get(index).getEnhancementModifier( + "");
+	numberField.setText(appController.getPokedex().get(index).getNumber() + "");
+	attackField.setText(appController.getPokedex().get(index).getAttackPoints() + "");
+	healthField.setText(appController.getPokedex().get(index).getHealthPoints() + "");
+	modifierField.setText(appController.getPokedex().get(index).getEnhancementModifier() + "");
+	}
+	
+	public PokemonPanel(PokemonController appController)
+	{
+		super();
+		healthlabel = new JLabel ("");
+		attackLabel = new JLabel ("");
+		nameLabel = new JLabel ("");
+		numberLabel = new JLabel ("");
+		evolvableLabel = new JLabel ("");
+		modifierLabel = new JLabel ("");
+		
+		evolvableBox = new JCheckBox ("");
+		nameField = new JTextField ("");
+		numberField = new JTextField ("");
+		attackField = new JTextField ("");
+		healthField = new JTextField ("");
+		modifierField = new JTextField ("");
+		
+		descriptionArea = new JTextArea ("");
+		typeArea = new JTextArea ("");
+		
+		saveButton = new JButton ("");
+		clearButton = new JButton ("");
+		pokedexDropdown = new JComboBox ("");
+		
+		
+	
+	}
+	public void setupPanel()
+	{
+		
+	}
 }
