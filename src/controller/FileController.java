ec7632c418051ab7d30236da76c03b5c515fef76
Sharp <rsha7868@740s-pl-127.local>
2018-01-17 12:54:29 -0700

    SAving the error that were fixed by the fileController.

--- src/controller/FileController.java
+++ src/controller/FileController.java
@@ -3,12 +3,16 @@ package controller;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import pokemon.model.*;
+import java.io.File;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+
 
 import pokemon.model.pokemonModel;
 
 public class FileController
 {
-	pulbic static void savePokemonToFile(ArrayList<pokemonModel> pokedex)
+	public static void savePokemonToFile(ArrayList<pokemonModel> pokedex)
 	{
 		File saveFile = new File ("Saved Pokedex.txt");
 		try
@@ -19,18 +23,38 @@ public class FileController
 			for (int index = 0; index < pokedex.size(); index++)
 			{
 				String name = pokedex.get(index).getName();
-				int number = pokedex.get(index).getNumebr();
+				int number = pokedex.get(index).getNumber();
 				int attack = pokedex.get(index).getAttackPoints();
 				int health = pokedex.get(index).getHealthPoints();
 				boolean evlove = pokedex.get(index).isCanEvolve();
-				double row = name + ","+ health + "," + "," + modify + "," + evolve;
+				double row = name + ","+ number + "," + health + "," + attack + "," + modify + "," + evolve + ",";
 				writeToDisk.println(row);
 			}
 			writeToDisk.close();
 		}
 		catch (FileNotFoundException error)
 		{
-			System.out.printlm("There was an error;" + error.getMessage);
+			System.out.println("There was an error;" + error.getMessage());
 		}
+	}
+	public static String readPokemonFromFile()
+	{
+	String contents = "";
+	String path = "Saved Pokedex.txt";
+	try
+	{
+		Scanner fileScanner = new Scanner(new File(path));
+		while (fileScanner.hasNextLine())
+		{
+			String row = fileScanner.nextLine();
+			contents +=	row + "\n";
+		}
+		fileScanner.close();
+	}
+	catch (FileNotFoundException error)
+	{
+		System.out.println("There was an error:" + error.getMessage());
+	}
+	return contents;
 	}
 }
